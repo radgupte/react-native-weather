3a1ca474490aa6e93f318c710555f5f39bb31955
Radhika Gupte <gupter@oregonstate.edu>
2023-12-06 20:10:57 -0800

    added content to the screen

--- App.js
+++ App.js
@@ -5,23 +5,57 @@ const App = () => {
   return (
     <SafeAreaView style={styles.droidSafeArea}>
       <View style={styles.container}>
-        <Text>Current Weather</Text>
+        <Text style={styles.temp}>6</Text>
+        <Text style={styles.feels}>Feels like 5</Text>
+        <View style={styles.highLowWrapper}>
+          <Text style={styles.highLow}>High: 8</Text>
+          <Text style={styles.highLow}>Low: 6</Text>
+        </View>
+      </View>
+      <View style={styles.bodyWrapper}>
+        <Text style={styles.description}>It's sunny</Text>
+        <Text style={styles.message}>It's a perfect T-shirt weather!</Text>
       </View>
     </SafeAreaView>
   );
 };
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'pink',
+  droidSafeArea: {
     flex: 1,
-    alignItems: 'center',
+    paddingTop: Platform.OS === 'android' ? 25 : 0,
+    backgroundColor: 'pink',
   },
   wrapper: {
     flex: 1,
   },
-  droidSafeArea: {
+  container: {
     flex: 1,
-    paddingTop: Platform.OS === 'android' ? 25 : 0,
+    alignItems: 'center',
+  },
+  temp: {
+    color: 'black',
+    fontSize: 48,
+  },
+  feels: {
+    color: 'black',
+    fontSize: 30,
+  },
+  highLow: {
+    color: 'black',
+    fontSize: 20,
+  },
+  highLowWrapper: {
+    flexDirection: 'row',
+  },
+  bodyWrapper: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
+  },
+  description: {
+    fontSize: 48,
+  },
+  message: {
+    fontSize: 30,
   },
 });
 
