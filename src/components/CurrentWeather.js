1edb0e7de0149eec2daead1c2f75737471dac2ab
Radhika Gupte <gupter@oregonstate.edu>
2023-12-06 20:22:03 -0800

    created a component and imported in App

---
+++ src/components/CurrentWeather.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+
+const CurrentWeather = () => {
+  return (
+    <SafeAreaView style={styles.droidSafeArea}>
+      <View style={styles.container}>
+        <Feather name="sun" size={100} color="black" />
+        <Text style={styles.temp}>6</Text>
+        <Text style={styles.feels}>Feels like 5</Text>
+        <View style={styles.highLowWrapper}>
+          <Text style={styles.highLow}>High: 8</Text>
+          <Text style={styles.highLow}>Low: 6</Text>
+        </View>
+      </View>
+      <View style={styles.bodyWrapper}>
+        <Text style={styles.description}>It's sunny</Text>
+        <Text style={styles.message}>It's a perfect T-shirt weather!</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+const styles = StyleSheet.create({
+  droidSafeArea: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? 25 : 0,
+    backgroundColor: 'pink',
+  },
+  wrapper: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  temp: {
+    color: 'black',
+    fontSize: 48,
+  },
+  feels: {
+    color: 'black',
+    fontSize: 30,
+  },
+  highLow: {
+    color: 'black',
+    fontSize: 20,
+  },
+  highLowWrapper: {
+    flexDirection: 'row',
+  },
+  bodyWrapper: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
+    paddingLeft: 25,
+    marginBottom: 14,
+  },
+  description: {
+    fontSize: 48,
+  },
+  message: {
+    fontSize: 30,
+  },
+});
+
+export default CurrentWeather;
