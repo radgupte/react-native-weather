ad0d96ed502c7a03c74e0207bd6955e92db0dc2f
Radhika Gupte <gupter@oregonstate.edu>
2023-12-10 20:59:58 -0800

    added location permission

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, ActivityIndicator } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Tabs from './src/components/Tabs';
+import * as Location from 'expo-location';
 
 const Tab = createBottomTabNavigator();
 
 const App = () => {
   const [loading, setLoading] = useState(true);
+  const [location, setLocation] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setError('permission to access location was denied');
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  if (location) {
+    console.log(location);
+  }
+
   if (loading) {
     return (
       <View style={styles.container}>
@@ -15,6 +35,7 @@ const App = () => {
       </View>
     );
   }
+
   return (
     <NavigationContainer>
       <Tabs />
