20538698d64d528ab4eaf9d69ca8625cf75b7225
Radhika Gupte <gupter@oregonstate.edu>
2023-12-10 19:24:27 -0800

    added loading state

--- App.js
+++ App.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { View, StyleSheet, ActivityIndicator } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Tabs from './src/components/Tabs';
-import Counter from './src/demonstration/Counter';
 
 const Tab = createBottomTabNavigator();
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size={'large'} color={'blue'} />
+      </View>
+    );
+  }
   return (
     <NavigationContainer>
-      {/* <Tabs /> */}
-      <Counter />
+      <Tabs />
     </NavigationContainer>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
 export default App;
