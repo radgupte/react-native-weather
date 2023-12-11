8f4a09cafc776f547037cb4bc6a50100d1a3064e
Radhika Gupte <gupter@oregonstate.edu>
2023-12-11 00:36:22 -0800

    updated upcoming weather component to use the data

--- src/screens/UpcomingWeather.js
+++ src/screens/UpcomingWeather.js
@@ -2,53 +2,13 @@ import React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  Text,
   FlatList,
   StatusBar,
   ImageBackground,
 } from 'react-native';
 import ListItem from '../components/ListItem';
 
-const DATA = [
-  {
-    dt_txt: '2022-08-30 16:00:00',
-    main: {
-      temp_min: 296.34,
-      temp_max: 298.24,
-    },
-    weather: [
-      {
-        main: 'Rain',
-      },
-    ],
-  },
-  {
-    dt_txt: '2022-08-30 17:00:00',
-    main: {
-      temp_min: 296.2,
-      temp_max: 296.31,
-    },
-    weather: [
-      {
-        main: 'Rain',
-      },
-    ],
-  },
-  {
-    dt_txt: '2022-08-30 18:00:00',
-    main: {
-      temp_min: 292.84,
-      temp_max: 294.94,
-    },
-    weather: [
-      {
-        main: 'Rain',
-      },
-    ],
-  },
-];
-
-const UpcomingWeather = () => {
+const UpcomingWeather = ({ weatherData }) => {
   const renderItem = ({ item }) => (
     <ListItem
       condition={item.weather[0].main}
@@ -61,10 +21,8 @@ const UpcomingWeather = () => {
   return (
     <SafeAreaView style={droidSafeArea}>
       <ImageBackground source={require('../../assets/rain.jpg')} style={image}>
-        <Text>Upcoming Weather</Text>
-
         <FlatList
-          data={DATA}
+          data={weatherData}
           renderItem={renderItem}
           keyExtractor={(item) => item.dt_txt}
         />
