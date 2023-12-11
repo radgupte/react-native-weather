4e1b53491e5401f2011cdb8ca3d52edb64b96129
Radhika Gupte <gupter@oregonstate.edu>
2023-12-11 00:03:13 -0800

    updated current weather component to use the data

--- App.js
+++ App.js
@@ -13,18 +13,18 @@ const App = () => {
   const [loading, error, weather] = useGetWeather();
   console.log(loading, error, weather);
 
-  if (loading) {
+  if (weather && weather.list) {
     return (
-      <View style={styles.container}>
-        <ActivityIndicator size={'large'} color={'blue'} />
-      </View>
+      <NavigationContainer>
+        <Tabs weather={weather} />
+      </NavigationContainer>
     );
   }
 
   return (
-    <NavigationContainer>
-      <Tabs />
-    </NavigationContainer>
+    <View style={styles.container}>
+      <ActivityIndicator size={'large'} color={'blue'} />
+    </View>
   );
 };
 
